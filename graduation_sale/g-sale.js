0099e3ca9f43cabfde91533073b0c3f1ce2a1d34
Haoyan Su <hs2949@nyu.edu>
2016-12-30 21:59:37 -0500

    Add files via upload

--- graduation_sale/g-sale.js
+++ graduation_sale/g-sale.js
@@ -37,58 +37,89 @@
 	function getCategory(){
 		var category;
 		$('sidebar')[0].addEventListener('click',function(e){
-			console.log("getCategory: "+e.target.className.split(" ",1));
-			category = e.target.className.split(" ",1);
-			console.log("设置了eventlistener: "+category);
-			if (id('selected') !== null && hasClass(category,id('selected')) === null ){
-					id('selected').id = "unselected";
+			if (e.target !== $('sidebar')[0]) {
+				console.log("getCategory: "+e.target.className.split(" ",1));
+				category = e.target.className.split(" ",1);
+				console.log("设置了eventlistener: "+category);
+				if (id('selected') !== null && hasClass(category,id('selected')) === null ){
+						id('selected').id = "unselected";
+						$(category)[0].id = "selected";
+					// console.log(id('unselected'));
+					
+				} 
+				if (id('selected')===null) {
 					$(category)[0].id = "selected";
-				// console.log(id('unselected'));
-				
-			} 
-			if (id('selected')===null) {
-				$(category)[0].id = "selected";
+				}
+				// console.log("执行判断sale_list中是否为空或重复的代码 第二个判断值为: ", id('sale_list').childNodes[1].className.match(category)!==null);
+				// var mydata = JSON.parse(mydata);
+				if (id('sale_list').childNodes.length !==1) {
+					// var jsonSentence = "mydata."+category+".length";
+					id('sale_list').innerHTML = "";
+				}
+				var dataCategory = formatData(category);
+				// console.log( dataLength[0].price);
+				for (var i = 0; i < dataCategory.length; i++) {
+					var new_product = document.createElement('li');
+					new_product.innerHTML = dataCategory[i].name + "        价格："+dataCategory[i].price;
+					new_product.id = dataCategory[i].id;
+					new_product.className = category+" items";
+					id('sale_list').appendChild(new_product);
+					console.log("have run new_product :"+new_product.id);
+					var p_description = document.createElement('div');
+					var pic01 = dataCategory[i].pic_url[0].pic;
+					var pic02 = dataCategory[i].pic_url[1].pic;
+					var pic03 = dataCategory[i].pic_url[2].pic;
+					// p_description.innerHTML = "<br><img src=\""+pic01 +"\" onerror=\"ChangeFileType(\""+pic01+"\")\">"+"<br><img src=\""+pic02 +"\" onerror=\"ChangeFileType("+pic02+")\">"+"<br><img src=\""+pic03 +"\" onerror=\"ChangeFileType("+pic03+")\">"+"<br>" + "产品描述：<div class=\"description\">" + dataCategory[i].description +"</div>"+ "<br><br>";
+					for (var j = 0; j < dataCategory[i].pic_url.length; j++) {
+						var descrip_img = document.createElement('IMG');
+						descrip_img.src = dataCategory[i].pic_url[j].pic;
+						p_description.appendChild(descrip_img);
+						console.log("address  "+dataCategory[i].pic_url[j].pic);
+						// p_description.innerHTML += "<br><img src=\"" + dataCategory[i].pic_url[j].pic + "\">";
+					}
+					var descrip_word = document.createElement('div');
+					descrip_word.innerHTML = "产品描述：<div class=\"description\">" + dataCategory[i].description +"</div>"+ "<br><br>";
+					p_description.appendChild(descrip_word);
+					p_description.style.display = 'none';
+					new_product.appendChild(p_description);			
+				}
+				console.log("set category");
 			}
-			// console.log("执行判断sale_list中是否为空或重复的代码 第二个判断值为: ", id('sale_list').childNodes[1].className.match(category)!==null);
-			// var mydata = JSON.parse(mydata);
-			if (id('sale_list').childNodes.length !==1) {
-				// var jsonSentence = "mydata."+category+".length";
-				id('sale_list').innerHTML = "";
-			}
-			var dataCategory = formatData(category);
-			// console.log( dataLength[0].price);
-			for (var i = 0; i < dataCategory.length; i++) {
-				var new_product = document.createElement('li');
-				new_product.innerHTML = dataCategory[i].name + "        价格："+dataCategory[i].price;
-				new_product.id = dataCategory[i].id;
-				new_product.className = category+" items";
-				id('sale_list').appendChild(new_product);
-				console.log("have run new_product :"+new_product.id);
-				var p_description = document.createElement('div');
-				p_description.innerHTML = "<br><img src=\""+dataCategory[i].pic_url[0].pic01 +"\">"+"<br><img src=\""+dataCategory[i].pic_url[1].pic02 +"\">"+"<br><img src=\""+dataCategory[i].pic_url[2].pic03 +"\">"+"<br>" + "产品描述：<div class=\"description\">" + dataCategory[i].description +"</div>"+ "<br><br>";
-				p_description.style.display = 'none';
-				new_product.appendChild(p_description);			
-			}
-			console.log("set category");
+			
 		},false);
 		setThingsListClick();
 		// console.log("category 值为： "+ category);
 		
 	}
 
+	function ChangeFileType(pic){
+		alert("cant load");
+		if (pic.indexOf("JPG")) {
+			pic = pic.slice(0,-3) + ".jpg";
+		}else{
+			pic = pic.toUpperCase();
+		}
+	}
+	
+
 	
 	function setThingsListClick(){
 		console.log('test');
 		id("sale_list").addEventListener('click',function(event){			
 			var description = event.target.childNodes[1];
-			console.log("description childNodes[1] "+description.innerHTML);
-			if (description.style.display === "none") {
-				description.style.display = "block";
-				console.log("let it show" + description.style.display);
-			}else{
-				description.style.display = "none";
-				console.log("let it hide");
+			// console.log("description childNodes[1] "+event.target.innerHTML);
+			if (event.target !== id('sale_list')) {
+				console.log("normal");
+				if (description.style.display === "none") {
+					description.style.display = "block";
+					console.log("let it show" + description.style.display);
+				}else{
+					description.style.display = "none";
+					console.log("let it hide");
+				}
+
 			}
+			
 			
 		},false);
 	}
